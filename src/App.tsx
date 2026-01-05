5348b44578651218d5dc4f0193712e6e74b540f0
OsamaMJux <arcdesignss@outlook.com>
2026-01-05 02:16:00 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import WhatsAppSticky from "./components/WhatsAppSticky";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -26,8 +27,20 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
+       <WhatsAppSticky />
+
+
+
+
+
     </TooltipProvider>
   </QueryClientProvider>
 );
 
+
+
+
 export default App;
+
+
